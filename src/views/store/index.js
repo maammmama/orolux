a283c9fefaf0816dcc3dd4b1d332a46c83f33e06
maammmama <hamzaamriki582@gmail.com>
2025-05-31 15:16:17 +0100

    transfor fiels

--- src/views/store/index.js
+++
@@ -1,6 +0,0 @@
-// Import store HTML content
-const storeHTML = await fetch('./src/views/store/index.html').then(res => res.text());
-
-export function loadStoreContent() {
-    return storeHTML;
-}
